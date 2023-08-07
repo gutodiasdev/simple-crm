c67725e833e734d45f896abf731cff19a64a714b
gutodiasux <gutodiasux@gmail.com>
2023-08-07 19:02:39 -0300

    feat(service): add GenerateUUID interface as a contructor dependency in CreateUserService

--- src/modules/application/services/CreateUser.ts
+++ src/modules/application/services/CreateUser.ts
@@ -1,16 +1,25 @@
-import { FindUserByEmailRepository } from '@/modules/data/contracts'
-import { UserAlreadyExistsError } from '@/modules/domain/error'
-import { CreateUser } from '@/modules/domain/features'
+import { FindUserByEmailRepository, SaveUserRepository } from '@/modules/data/contracts'
+import { CreateUserError, UserAlreadyExistsError } from '@/modules/domain/error'
+import { CreateUser, GenerateUUID } from '@/modules/domain/features'
 
 export class CreateUserService implements CreateUser {
-    constructor(private readonly userRepository: FindUserByEmailRepository) {}
+    constructor(
+        private readonly userRepository: FindUserByEmailRepository & SaveUserRepository,
+        private readonly UUIDGeneratorService: GenerateUUID
+    ) { }
 
     async execute(input: CreateUser.Input): Promise<CreateUser.Ouput> {
         const user = await this.userRepository.findUserByEmail({ email: input.email })
         if (user) throw new UserAlreadyExistsError()
-        return {
-            token: '',
-            refreshToken: ''
+        const id = this.UUIDGeneratorService.execute()
+        try {
+            await this.userRepository.saveUser({...input, id: id})
+            return {
+                token: '',
+                refreshToken: ''
+            }
+        } catch (error: any) {
+            throw new CreateUserError(error.stack)
         }
     }
 }
