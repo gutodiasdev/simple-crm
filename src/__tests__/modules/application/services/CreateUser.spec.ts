a5229e488d37ae41569c42ed775467eebd7983ed
gutodiasux <gutodiasux@gmail.com>
2023-08-07 18:51:38 -0300

    test: ensure saveUser method is called with correct params

--- src/__tests__/modules/application/services/CreateUser.spec.ts
+++ src/__tests__/modules/application/services/CreateUser.spec.ts
@@ -1,10 +1,12 @@
 import { CreateUserService } from '@/modules/application/services'
 import { FindUserByEmailRepository, SaveUserRepository } from '@/modules/data/contracts'
 import { UserAlreadyExistsError } from '@/modules/domain/error'
+import { GenerateUUID } from '@/modules/domain/features'
 import { MockProxy, mock } from 'jest-mock-extended'
 
 describe('CreateUserService', () => {
     let userRepository: MockProxy<FindUserByEmailRepository & SaveUserRepository>
+    let generateUUIDService: MockProxy<GenerateUUID>
     let sut: CreateUserService
     const input = {
         email: 'any_email',
@@ -14,8 +16,10 @@ describe('CreateUserService', () => {
 
     beforeEach(() => {
         userRepository = mock()
-        sut = new CreateUserService(userRepository)
+        generateUUIDService = mock()
+        sut = new CreateUserService(userRepository, generateUUIDService)
         userRepository.findUserByEmail.mockResolvedValue(undefined)
+        generateUUIDService.execute.mockReturnValue('any_id')
     })
 
     test('it should CreateUserService returns token and refreshToken', async () => {
@@ -28,4 +32,13 @@ describe('CreateUserService', () => {
         await expect(() => sut.execute(input)).rejects.toThrow(UserAlreadyExistsError)
     })
     
+    test('it should saveUser method is called with correct params', async () => {
+        await sut.execute(input)
+        expect(userRepository.saveUser).toHaveBeenCalledWith({
+            id: 'any_id',
+            email: 'any_email',
+            password: 'any_password',
+            agreeWithPolicies: true
+        })
+    })
 })
